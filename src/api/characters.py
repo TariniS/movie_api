4eaf2e3f8dbaa2edf4e02dcf89be00bd7321a9cd
TariniS <tareene@gmail.com>
2023-04-07 22:17:26 -0700

    endpoint1 working

--- src/api/characters.py
+++ src/api/characters.py
@@ -26,16 +26,80 @@ def get_character(id: str):
     * `number_of_lines_together`: The number of lines the character has with the
       originally queried character.
     """
-    for character in db.characters:
-        if character["character_id"] == id:
-            print("character found")
+    conversations_list_ids = set()
+    conversations_list_lines = dict()
 
-    json = None
+    total_conversations = dict()
+    x = None
 
-    if json is None:
+    character_id = id
+    
+    if character_id in db.characters.keys():
+      character_name = db.characters[character_id]['name']
+      movie_name = db.movies[db.characters[character_id]['movie_id']]['title']
+      gender = db.characters[character_id]['gender']
+      if gender == '':
+          gender = None
+
+      if character_id in db.conversations_dict.keys():
+        for conversation in db.conversations_dict[character_id]:
+          conversations_list_ids.add(conversation["character2_id"])
+          if conversation["character2_id"] in total_conversations.keys():
+            val: list = total_conversations[conversation["character2_id"]]
+            val.append(conversation["conversation_id"])
+            total_conversations[conversation["character2_id"]] = val
+          else:
+            total_conversations[conversation["character2_id"]] = [conversation["conversation_id"]]
+
+          
+      if character_id in db.conversations_dict2.keys():
+        for conversation in db.conversations_dict2[character_id]:
+          conversations_list_ids.add(conversation["character1_id"])
+          if conversation["character1_id"] in total_conversations.keys():
+            val: list = total_conversations[conversation["character1_id"]]
+            val.append(conversation["conversation_id"])
+            total_conversations[conversation["character1_id"]] = val
+          else:
+            total_conversations[conversation["character1_id"]] = [conversation["conversation_id"]]
+
+
+      for charId in total_conversations:
+        listConvoIds = total_conversations[charId]
+        sum = 0
+        for convoId in listConvoIds:
+          sum = sum + len(db.lines_dict[convoId])
+        conversations_list_lines[charId] = sum
+      
+
+      top_conversationsSorted = sorted(conversations_list_lines.items(), key=lambda x: (-x[1], x[0]))
+      sortedFinal = dict(top_conversationsSorted)
+      
+      top_convosJson = []
+
+      for charId in sortedFinal.keys():
+        charName = db.characters[charId]['name']
+        gen = db.characters[charId]['gender']
+        if gen == '':
+          gen = None
+        numLines = sortedFinal[charId]
+        val = {
+          "character_id":int(charId), 
+          "character":charName, 
+          "gender":gen, 
+          "number_of_lines_together": numLines
+        }
+        top_convosJson.append(val)
+      x = {
+        "character_id": int(character_id),
+        "character": character_name,
+        "movie": movie_name,
+        "gender": gender,
+        "top_conversations": top_convosJson
+      }
+    if x is None:
         raise HTTPException(status_code=404, detail="movie not found.")
 
-    return json
+    return x
 
 
 class character_sort_options(str, Enum):
@@ -51,6 +115,9 @@ def list_characters(
     offset: int = 0,
     sort: character_sort_options = character_sort_options.character,
 ):
+
+
+
     """
     This endpoint returns a list of characters. For each character it returns:
     * `character_id`: the internal id of the character. Can be used to query the
@@ -58,6 +125,8 @@ def list_characters(
     * `character`: The name of the character.
     * `movie`: The movie the character is from.
     * `number_of_lines`: The number of lines the character has in the movie.
+
+    
 
     You can filter for characters whose name contains a string by using the
     `name` query parameter.
@@ -72,6 +141,15 @@ def list_characters(
     maximum number of results to return. The `offset` query parameter specifies the
     number of results to skip before returning results.
     """
+
+    # myKeys = list(db.characters_dict.keys())
+    # myKeys.sort()
+    # sorted_dict = {i: db.characters_dict[i] for i in myKeys}
+    # print(list(sorted_dict.items())[:50])
+
+
+    # print("names")
+    # print(db.character_names)
 
     json = None
     return json
--- src/database.py
+++ src/database.py
@@ -6,17 +6,33 @@ import csv
 
 print("reading movies")
 
+# with open("movies.csv", mode="r", encoding="utf8") as csv_file:
+#     movies = [
+#         {k: v for k, v in row.items()}
+#         for row in csv.DictReader(csv_file, skipinitialspace=True)
+#     ]
+
 with open("movies.csv", mode="r", encoding="utf8") as csv_file:
-    movies = [
-        {k: v for k, v in row.items()}
-        for row in csv.DictReader(csv_file, skipinitialspace=True)
-    ]
+    reader = csv.DictReader(csv_file)
+    movies = {row.pop('movie_id'): row for row in reader}
+
+# with open("characters.csv", mode="r", encoding="utf8") as csv_file:
+#     characters2 = [
+#         {k: v for k, v in row.items()}
+#         for row in csv.DictReader(csv_file, skipinitialspace=True)
+#     ]
 
 with open("characters.csv", mode="r", encoding="utf8") as csv_file:
-    characters = [
-        {k: v for k, v in row.items()}
-        for row in csv.DictReader(csv_file, skipinitialspace=True)
-    ]
+    reader = csv.DictReader(csv_file)
+    characters = {row.pop('character_id'): row for row in reader}
+
+
+
+
+# with open("characters.csv", mode="r", encoding="utf8") as csv_file:
+#     reader = csv.DictReader(csv_file)
+#     characters_names = {row.pop('name'): row for row in reader}
+
 
 with open("conversations.csv", mode="r", encoding="utf8") as csv_file:
     conversations = [
@@ -24,8 +40,92 @@ with open("conversations.csv", mode="r", encoding="utf8") as csv_file:
         for row in csv.DictReader(csv_file, skipinitialspace=True)
     ]
 
+with open("conversations.csv", mode="r", encoding="utf8") as csv_file:
+    conversations2 = [
+    {k: v for k, v in row.items()}
+    for row in csv.DictReader(csv_file, skipinitialspace=True)
+]
+
+# with open("conversations.csv", mode="r", encoding="utf8") as csv_file:
+#     reader = csv.DictReader(csv_file)
+#     conversations = {row.pop('character1_id'): row for row in reader}
+
 with open("lines.csv", mode="r", encoding="utf8") as csv_file:
     lines = [
         {k: v for k, v in row.items()}
         for row in csv.DictReader(csv_file, skipinitialspace=True)
     ]
+
+
+# with open("lines.csv", mode="r", encoding="utf8") as csv_file:
+#     reader = csv.DictReader(csv_file)
+#     lines = {row.pop('conversation_id'): row for row in reader}
+  
+# re read in all of them to make them into dictionary by key of movie id
+            
+# can combine characters and movie information through movie id
+# for each movie id, t
+# character id, movie id as keys - > movie information, conversations based on character id, lines based on conversations
+# for each character the top conversations needs to be 
+# conversations based on character id -> another character id
+# for those conversations can combine into number of lines
+# 
+
+
+# character i1_id. 
+
+
+character_names = dict()
+
+for id in characters:
+    name = characters[id]['name']
+    if name not in character_names.keys():
+        character_names[name] = [characters[id]]
+    else:
+        val: list = character_names[name] 
+        val.append(characters[id])
+        character_names[name] = val
+
+print("names")
+print(character_names)
+
+
+lines_dict = dict()
+
+for line in lines:
+    conversationId = line["conversation_id"]
+    if conversationId not in lines_dict.keys():
+        # no character 1 information is found, then add
+        lines_dict[conversationId] = [line]
+    else:
+        val: list = lines_dict[conversationId] 
+        val.append(line)
+        lines_dict[conversationId] = val
+
+
+
+conversations_dict = dict()
+for conversation in conversations:
+    character1ID = conversation["character1_id"]
+    if character1ID not in conversations_dict.keys():
+        # no character 1 information is found, then add
+        conversations_dict[character1ID] = [conversation]
+    else:
+        val: list = conversations_dict[character1ID] 
+        val.append(conversation)
+        conversations_dict[character1ID] = val
+
+conversations_dict2 = dict()
+for conversation in conversations2:
+    character2ID = conversation["character2_id"]
+    if character2ID not in conversations_dict2.keys():
+        # no character 1 information is found, then add
+        conversations_dict2[character2ID] = [conversation]
+    else:
+        val: list = conversations_dict2[character2ID] 
+        val.append(conversation)
+        conversations_dict2[character2ID] = val
+
+
+
+
